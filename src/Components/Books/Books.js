adef3f78b0469ec1f7ffaadc0bf29e33ceb6cb31
tonyg <aj.gianno@gmail.com>
2021-04-16 21:05:26 -0500

    api url

--- src/Components/Books/Books.js
+++ src/Components/Books/Books.js
@@ -29,7 +29,6 @@ class Books extends Component {
     e.preventDefault();
     let key = 'AIzaSyCZpwG98Mk3oQOoAClpWwcXi28JoV9pqhQ';
     let url = `https://www.googleapis.com/books/v1/volumes?q=${this.state.searchField}&printType=${this.state.printType}&filter=${this.state.filter}&key=${key}`;
-    // let url = `https://www.googleapis.com/books/v1/volumes?q=${this.state.searchField}&key=${key}`;
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
